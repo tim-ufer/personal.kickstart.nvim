5a7d911e0cfcca3d9b50de52d315a2af99b4a944
Tim Ufer <tim.ufer98@gmail.com>
2024-11-10 19:54:40 +0100

    synced fork, enabled nerd font, file % etc; added lazylock

--- init.lua
+++ init.lua
@@ -923,7 +923,7 @@ require('lazy').setup({
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return '%2l:%-2v|%P'
       end
 
       -- ... and there is more!
